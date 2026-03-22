78a92cc229ff6daee5f5a92bd083a78af66ed445
sajmon-55 <szgodlewski.pl@gmail.com>
2026-03-22 10:59:52 +0100

    add Rental class

---
+++ APBD-Cw2/Models/Rental.cs
@@ -0,0 +1,19 @@
+﻿namespace APBD_Cw2.Models;
+
+public class Rental(Equipment equipment, User user, DateTime from, DateTime to)
+{
+    private static int _nextId = 1;
+
+    public int Id { get; } = _nextId++;
+    public Equipment Equipment { get; } = equipment;
+    public User User { get; } = user;
+    public DateTime From { get; } = from >= to ? throw new ArgumentException("Invalid time range") : from;
+    public DateTime To { get; } = to;
+    public bool isCancelled { get; private set; } = false;
+    
+    public void Cancel()
+    {
+        isCancelled = true;
+    }
+    
+}
