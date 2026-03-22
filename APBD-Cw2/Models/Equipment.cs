7aee99b4c4bdf253a60e6e5884c5a4a8807ef3f6
sajmon-55 <szgodlewski.pl@gmail.com>
2026-03-22 10:37:02 +0100

    add base Equipment class

---
+++ APBD-Cw2/Models/Equipment.cs
@@ -0,0 +1,13 @@
+﻿using APBD_Cw2.Enums;
+
+namespace APBD_Cw2.Models;
+
+public abstract class Equipment(string name, EquipmentType equipmentType)
+{
+    private static int _nextId = 1;
+    
+    public int Id { get; } = _nextId++;
+    public string Name { get; set; } = name;
+    public EquipmentStatus Status { get; set; } = EquipmentStatus.Available;
+    public EquipmentType Type { get; } = equipmentType;
+}
