76139ad82efabe5e58dfdabc65924ad4819c2ce1
sajmon-55 <szgodlewski.pl@gmail.com>
2026-03-22 10:55:36 +0100

    add base User class

---
+++ APBD-Cw2/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using APBD_Cw2.Enums;
+
+namespace APBD_Cw2.Models;
+
+public abstract class User(string firstName, string lastName, UserType userType)
+{
+    private static int _nextId = 1;
+    
+    public int Id { get; } = _nextId++;
+    public string FirstName { get; } = firstName;
+    public string LastName { get; } = lastName;
+    public UserType UserType { get; } = userType;
+}
