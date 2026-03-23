19798fd80902e5711a107f33f00a2edc02125971
sajmon-55 <szgodlewski.pl@gmail.com>
2026-03-23 19:14:28 +0100

    add Equipment service

---
+++ APBD-Cw2/Services/Equipments/EquipmentService.cs
@@ -0,0 +1,46 @@
+﻿using APBD_Cw2.Enums;
+using APBD_Cw2.Exceptions;
+using APBD_Cw2.Models;
+
+namespace APBD_Cw2.Services.Equipments;
+
+public class EquipmentService : IEquipmentService
+{
+    private readonly List<Equipment> _equipments = [];
+    
+    public void AddEquipment(Equipment equipment)
+    {
+        _equipments.Add(equipment);
+    }
+
+    public Equipment GetEquipmentById(int equipmentId)
+    {
+        return _equipments.FirstOrDefault(x => x.Id == equipmentId)
+            ?? throw new EquipmentNotFoundException(equipmentId);
+    }
+
+    public List<Equipment> GetAll()
+    {
+        return _equipments;
+    }
+
+    public List<Equipment> GetAllByType(EquipmentType equipmentType)
+    {
+        return _equipments.Where(e => e.Type == equipmentType).ToList();
+    }
+
+    public List<Equipment> GetAvailable()
+    {
+        return _equipments.Where(e => e.Status == EquipmentStatus.Available).ToList();
+    }
+
+    public void SetAvailable(int equipmentId)
+    {
+        GetEquipmentById(equipmentId).Status = EquipmentStatus.Available;
+    }
+
+    public void SetUnavailable(int equipmentId)
+    {
+        GetEquipmentById(equipmentId).Status = EquipmentStatus.Unavailable;
+    }
+}
---
+++ APBD-Cw2/Services/Equipments/IEquipmentService.cs
@@ -0,0 +1,16 @@
+﻿using APBD_Cw2.Enums;
+using APBD_Cw2.Models;
+
+namespace APBD_Cw2.Services.Equipments;
+
+public interface IEquipmentService
+{
+    public void AddEquipment(Equipment equipment);
+    public Equipment GetEquipmentById(int equipmentId);
+    public List<Equipment> GetAll();
+    public List<Equipment> GetAllByType(EquipmentType equipmentType);
+    public List<Equipment> GetAvailable();
+    public void SetAvailable(int equipmentId);
+    public void SetUnavailable(int equipmentId);
+    
+}
