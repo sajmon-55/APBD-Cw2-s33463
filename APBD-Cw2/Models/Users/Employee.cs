016f73ae5c0e5083c6b866022c92fd6847c56114
sajmon-55 <szgodlewski.pl@gmail.com>
2026-03-24 14:53:28 +0100

    Update Employee.cs

--- APBD-Cw2/Models/Users/Employee.cs
+++ APBD-Cw2/Models/Users/Employee.cs
@@ -4,5 +4,5 @@ namespace APBD_Cw2.Models.Users;
 
 public class Employee(string firstName, string lastName) : User (firstName, lastName, UserType.Employee)
 {
-    
+    public override int GetMaxRentals() => 2;
 }
