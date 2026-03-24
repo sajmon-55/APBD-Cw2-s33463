93713a9777f20c5ca6c1e7f28ee4a4f0c7d1c617
sajmon-55 <szgodlewski.pl@gmail.com>
2026-03-24 14:53:09 +0100

    Create UserService.cs

---
+++ APBD-Cw2/Services/Users/UserService.cs
@@ -0,0 +1,13 @@
+﻿using APBD_Cw2.Models;
+
+namespace APBD_Cw2.Services.Users;
+
+public class UserService : IUserService
+{
+    private readonly List<User> _users = [];
+
+    public void AddUser(User user)
+    {
+        _users.Add(user);
+    }
+}
