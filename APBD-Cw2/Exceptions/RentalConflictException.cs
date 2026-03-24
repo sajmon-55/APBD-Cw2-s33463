23192926f23a4b153bbe488a455837f458619305
sajmon-55 <szgodlewski.pl@gmail.com>
2026-03-24 14:53:40 +0100

    Create RentalConflictException.cs

---
+++ APBD-Cw2/Exceptions/RentalConflictException.cs
@@ -0,0 +1,4 @@
+﻿namespace APBD_Cw2.Exceptions;
+
+public class RentalConflictException(int equipmentId, DateTime from, DateTime to) 
+    :  Exception($"Equipment {equipmentId} is already reserved for the period from {from} to {to}.");
